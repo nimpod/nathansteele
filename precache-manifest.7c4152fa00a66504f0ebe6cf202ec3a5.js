13dccf4ea38fb7072184f6ad45163dee1d23b0ee
Nathan Steele <nathanjoshuasteele@gmail.com>
2020-09-19 18:50:36 +0100

    Updates

---
+++ precache-manifest.7c4152fa00a66504f0ebe6cf202ec3a5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a1f24ab0fedf6723e247d2c7bed7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df058d63590766bee25e",
+    "url": "/static/css/main.9124d03c.chunk.css"
+  },
+  {
+    "revision": "ce3e2f839f35e252f2ab",
+    "url": "/static/js/2.1d8758cc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.1d8758cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df058d63590766bee25e",
+    "url": "/static/js/main.135fd6a5.chunk.js"
+  },
+  {
+    "revision": "f1aff2991c89a40b8077",
+    "url": "/static/js/runtime-main.1f3214f1.js"
+  },
+  {
+    "revision": "13aa582ea645c6e6d6753ae419ed1475",
+    "url": "/static/media/arrowLeft.13aa582e.svg"
+  },
+  {
+    "revision": "9533dabb82761e4e0974f87d653c0b63",
+    "url": "/static/media/assembly.9533dabb.png"
+  },
+  {
+    "revision": "9a12ebd2d34d4b6a671ec7d2e314de15",
+    "url": "/static/media/bobomb.9a12ebd2.jpg"
+  },
+  {
+    "revision": "cd18b83b6a119a8277e04a4b10c6ff1b",
+    "url": "/static/media/calendar.cd18b83b.svg"
+  },
+  {
+    "revision": "51361c221833850ee6665da4f3062aca",
+    "url": "/static/media/cv.51361c22.pdf"
+  },
+  {
+    "revision": "6f9afe9dcfb0584e595a0b4b062c4c6d",
+    "url": "/static/media/d3.6f9afe9d.PNG"
+  },
+  {
+    "revision": "b03b40be2b3f6af3925e003efd00a2f3",
+    "url": "/static/media/fitbit-data-analysis.b03b40be.PNG"
+  },
+  {
+    "revision": "78f51c9b4a4adb87cdce0baf65114029",
+    "url": "/static/media/flappy_bird.78f51c9b.png"
+  },
+  {
+    "revision": "642adad69434a5be7f2fe90fc1b4b966",
+    "url": "/static/media/grid.642adad6.svg"
+  },
+  {
+    "revision": "1c5ae1f6276139800f055961469a7468",
+    "url": "/static/media/list.1c5ae1f6.svg"
+  },
+  {
+    "revision": "c42da9657bb925dfd9bf4fbba8249e26",
+    "url": "/static/media/logo.c42da965.svg"
+  },
+  {
+    "revision": "8ad6d4dcd74c1954519d6320c8bf46af",
+    "url": "/static/media/me.8ad6d4dc.jpg"
+  },
+  {
+    "revision": "a858265c3ed3f75c360c98b44f90359a",
+    "url": "/static/media/mosaics-of-videos.a858265c.png"
+  },
+  {
+    "revision": "d63300bcb1a8e5d31a4e3178b62153aa",
+    "url": "/static/media/pc.d63300bc.jpg"
+  },
+  {
+    "revision": "4becf871287358fd3f65e36a754149df",
+    "url": "/static/media/user.4becf871.svg"
+  },
+  {
+    "revision": "7c75513d150f618bffc123d53afb7960",
+    "url": "/static/media/web-dev-thumbnail.7c75513d.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nathansteele/precache-manifest.5d39fcd62dc005a028019a14cd14c93f.js"
+  "/precache-manifest.7c4152fa00a66504f0ebe6cf202ec3a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nathansteele/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
