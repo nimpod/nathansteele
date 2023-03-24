a449452cf1ce4540942f12d8c590d59500ffd790
nimpod <nathanjoshuasteele@gmail.com>
2023-03-24 17:13:08 +0000

    insp

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -390,6 +390,171 @@ const initState = {
             tags: ["Python", "Running"]
         },
         {
+            id: "10",
+            date: "24 Mar 2023",
+            title: "Inspiration database",
+            content: "\
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Film reviewers</h3> \
+                    <ul> \
+                        <li><a href='https://letterboxd.com/onderhond/'>Niels Matthijs</a></li> \
+                        <li><a href='https://letterboxd.com/luis_989/'>Luis_989</a></li> \
+                        <li><a href='https://www.youtube.com/kermodeandmayostake'>Mark Kermode</a></li> \
+                        <li><a href='https://www.youtube.com/@deepfocuslens'>deepfocuslens</a></li> \
+                        <li><a href='https://letterboxd.com/sitenoise/'>sitenoise</a></li> \
+                        <li><a href='https://www.youtube.com/@HarrysMovingMedia'>Harrys Moving Castle</a></li> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Music reviewers</h3> \
+                    <ul> \
+                        <li><a href='https://www.youtube.com/@TheWonkyAngle'>The Wonky Angle</a></li> \
+                        <li><a href='https://www.youtube.com/@theneedledrop'>The Needle Drop</a></li> \
+                        <li><a href='https://rateyourmusic.com/~logstar100'>logstar100</a></li> \
+                        <li><a href='https://rateyourmusic.com/~Takashi_Hafuza'>Takashi_Hafuza</a></li> \
+                        <li><a href='https://www.youtube.com/@SpectrumPulseKMG'>Spectrum Pulse</a></li> \
+                        <li><a href='https://www.youtube.com/@deepcuts'>Deep Cuts</a></li> \
+                        <li><a href='https://www.youtube.com/@freetobewhoibe/featured'>freetobewhoibe</a></li> \
+                        <li><a href='https://www.youtube.com/@ShawnCee'>Shawn Cee</a></li> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Astronomy</h3> \
+                    <ul> \
+                        <li><a href='https://www.youtube.com/@geographics1340'>Geographics</a></li> \
+                        <li><a href='https://youtube.com/@whatdamath'>Anton Petrov</a></li> \
+                        <li><a href='https://youtube.com/@Drekslerr'>Dreksler Astral</a></li> \
+                        <li><a href='https://youtube.com/@sea_space'>SEA</a></li> \
+                        <li><a href='https://youtube.com/@SpaceRip'>Space Rip</a></li> \
+                        <li><a href='https://youtube.com/@SciShow'>Sci Show</a></li> \
+                        <li><a href='https://www.youtube.com/@scishowspace'>Sci Show Space</a></li> \
+                        <li><a href='https://youtube.com/@3blue1brown'>3Blue1Brown</a></li> \
+                        <li><a href='https://youtube.com/@frasercain'>Fraser Cain</a></li> \
+                        <li><a href='https://youtube.com/@ElderFoxDocumentaries'>Elderfox Documentaries</a></li> \
+                        <li><a href='https://youtube.com/@JohnMichaelGodier'>John Michael Godier</a></li> \
+                        <li><a href='https://youtube.com/@Kosmo_off'>Kosmo</a></li> \
+                        <li><a href='https://youtube.com/@melodysheep'>Melody Sheep</a></li> \
+                        <li><a href='https://www.youtube.com/@AhSharkee'>Sharkee</a></li> \
+                        <li><a href='https://youtube.com/@DrBecky'>Dr Becky</a></li> \
+                        <li><a href='https://youtube.com/@EventHorizonShow'>Event Horizon</a></li> \
+                        <li><a href='https://www.youtube.com/@AlwaysAsking'>AlwaysAsking</a></li> \
+                        <li><a href='https://youtube.com/@isaacarthur3209'>Issac Arhur</a></li> \
+                        <li><a href='https://youtube.com/@kurzgesagt'>Kurzgesagt</a></li> \
+                        <li><a href='https://youtube.com/@LEMMiNO'>LEMMiNO</a></li> \
+                        <li><a href='https://youtube.com/@MinutePhysics'>Minute Physics</a></li> \
+                        <li><a href='https://www.youtube.com/@ChrisPattisonCosmo'>Chris Pattison</a></li> \
+                        <li><a href='https://youtube.com/@CoolWorldsLab'>Cool Worlds</a></li> \
+                        <li><a href='https://youtube.com/@pbsspacetime'>PBS Space Time</a></li> \
+                        <li><a href='https://youtube.com/@AstroFocus'>Astro Focus</a></li> \
+                        <li><a href='https://www.youtube.com/@LaunchPadAstronomy'>Launch Pad Astronomy</a></li> \
+                        <li><a href='https://youtube.com/@conversationswithjoe'>Conversations With Joe</a></li> \
+                        <li><a href='https://youtube.com/@parallaxnick637'>Parallax Nick</a></li> \
+                        <li><a href='https://www.youtube.com/@StrangeMysteries'>Strange Mysteries</a></li> \
+                        <li><a href='https://www.youtube.com/@StarTalk'>Star Talk</a></li> \
+                        <li><a href='https://youtube.com/@V101SPACE'>V101 Space</a></li> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Music discovery</h3> \
+                    <ul> \
+                        <li><a href='https://www.youtube.com/@TerminalPassage'>Terminal Passage</a></li> \
+                        <li><a href='https://www.youtube.com/@SaturnArchives'>Terminal Passage</a></li> \
+                        <li><a href='https://www.youtube.com/@VaporMemory'>Vapor Memory</a></li> \
+                        <li><a href='https://www.youtube.com/@QuoteCaveStory'>OnAScaleof1to10</a></li> \
+                        <li><a href='https://www.youtube.com/@PsychedelicAngel'>Psychedlic Angel</a></li> \
+                        <li><a href='https://www.youtube.com/@CubensisRecords'>Cubensis Records</a></li> \
+                        <li><a href='https://www.youtube.com/@should_be_asleep'>should be asleep</a></li> \
+                        <li><a href='https://www.youtube.com/@imcyborgbutthatsok'>i'm cyborg but that's ok</a></li> \
+                        <li><a href='https://www.youtube.com/@yumyam'>yum yam</a></li> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Miscellaneous youtubers</h3> \
+                    <ul> \
+                        <li><a href='https://youtube.com/@CrackingTheNutshell'>Cracking the Nutshell</a> \
+                        <li><a href='https://www.youtube.com/@Vsauce'>Vsauce</a> \
+                        <li><a href='https://www.youtube.com/@veritasium'>Veritasium</a> \
+                        <li><a href='https://www.youtube.com/@Wendoverproductions'>Wendover Productions</a> \
+                        <li><a href='https://youtube.com/@RealLifeLore'>Real Life Lore</a> \
+                        <li><a href='https://www.youtube.com/@smartereveryday'>SmarterEveryDay</a> \
+                        <li><a href='https://www.youtube.com/@HybridLibrarian'>Hybrid Librarian</a> \
+                        <li><a href='https://www.youtube.com/@CGPGrey'>CGP Grey</a> \
+                        <li><a href='https://www.youtube.com/@ZuckThat'>ZuckThat</a> \
+                        <li><a href='https://www.youtube.com/@NatureVideoChannel'>Nature Video</a> \
+                        <li><a href='https://www.youtube.com/@ItsBloodyScience'>It's Bloody Science!</a> \
+                        <li><a href='https://www.youtube.com/@numberphile'>Numberphile</a> \
+                        <li><a href='https://www.youtube.com/@periodicvideos'>Periodic Videos</a> \
+                        <li><a href='https://www.youtube.com/@RealEngineering'>Real Engineering</a> \
+                        <li><a href='https://www.youtube.com/@TommyEdisonXP'>Tommy Edison</a> \
+                        <li><a href='https://www.youtube.com/@Computerphile'>Computerphile</a> \
+                        <li><a href='https://www.youtube.com/@standupmaths'>Stand-up Maths</a> \
+                        <li><a href='https://www.youtube.com/@Mathologer'>Mathologger</a> \
+                        <li><a href='https://www.youtube.com/@SimonClark'>Simon Clark</a> \
+                        <li><a href='https://www.youtube.com/@Vihart'>Vihart</a> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Running</h3> \
+                    <ul> \
+                        <li><a href='https://www.randomforestrunner.com/'>John Kelly</a> \
+                        <li><a href='https://www.youtube.com/@BenParkes'>Ben Parkes</a> \
+                        <li><a href='https://www.youtube.com/@SethJamesDeMoor'>Seth James DeMoor</a> \
+                        <li><a href='https://twitter.com/runcandice'>Candice</a> \
+                        <li><a href='https://twitter.com/runcamille'>Camille Herron</a> \
+                        <li><a href='https://www.youtube.com/@stephbruce6177'>Steph Bruce</a> \
+                        <li><a href='https://www.youtube.com/@runningchannel'>The Running Channel</a> \
+                        <li><a href='https://www.youtube.com/@TheGingerRunner'>The Ginger Runner</a> \
+                        <li><a href='https://twitter.com/mikewardian'>Mike Wardian</a> \
+                        <li><a href='https://www.instagram.com/bygolly.molly/?hl=en'>Molly Seidel</a> \
+                        <li><a href='https://www.youtube.com/@HarryRunsUK'>Harry Runs</a> \
+                        <li><a href='https://www.youtube.com/@HellahGood'>Hellah Good</a> \
+                        <li><a href='https://www.youtube.com/@HybridCalisthenics'>Hybrid Calisthenics</a> \
+                        <li><a href='https://www.youtube.com/@yogawithadriene'>Yoga With Adriene</a> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Outdoors</h3> \
+                    <ul> \
+                        <li><a href='https://www.youtube.com/@jkynaston'>John Kynaston</a> \
+                        <li><a href='https://www.youtube.com/@MediocreAmateur'>Mediocre Amateur</a> \
+                        <li><a href='https://www.youtube.com/@kraigadams'>Kraig Adams</a> \
+                        <li><a href='https://www.youtube.com/@TheUnlazyWay'>The Unlazy Way</a> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Cycling</h3> \
+                    <ul> \
+                        <li><a href='https://www.youtube.com/@Cade_Media'>Cade Media</a> \
+                        <li><a href='https://www.youtube.com/@Cameron_Jeffers'>Cameron Jeffers</a> \
+                        <li><a href='https://www.youtube.com/@1bike1world'>1bike1world</a> \
+                        <li><a href='https://www.youtube.com/@BikeTripping'>Bike Tripping</a> \
+                    </ul> \
+                </div> \
+                <div class='blog-subsection'> \
+                    <h3 class='subgroup-title'>Artists</h3> \
+                    <ul> \
+                        <li><a href='https://www.joemacgown.com/'>Joe Macgown</a> \
+                        <li><a href='https://www.google.com/search?q=ojimbo&client=firefox-b-d&sxsrf=AJOqlzW2Cq-tlvV2ev7p3YGKkAScYbr7sQ:1679670949703&source=lnms&tbm=isch&sa=X&ved=2ahUKEwigrK_y7fT9AhWBWMAKHV_qBpsQ_AUoAXoECAEQAw&biw=1857&bih=967&dpr=1'>ojimbo</a> \
+                        <li><a href='https://twitter.com/TormanJeremy'>Jermey Torman</a> \
+                        <li><a href='https://twitter.com/DrewMedina20'>DrewMedina</a> \
+                        <li><a href='https://twitter.com/jakejfried'>Jake Fried</a> \
+                        <li><a href='https://twitter.com/seerlight'>SeerLight</a> \
+                        <li><a href='https://twitter.com/GlennIsZen'>Glenn Marshall</a> \
+                        <li><a href='https://twitter.com/AetherialDnB'>Aetherial.eth</a> \
+                        <li><a href='https://twitter.com/jeffpalmer'>Jeff Palmer</a> \
+                        <li><a href='https://twitter.com/zippy731'>Chris Allen</a> \
+                        <li><a href='https://twitter.com/pharmapsychotic'>pharmapsychotic</a> \
+                        <li><a href='https://twitter.com/garlicml'>garlicml</a> \
+                        <li><a href='https://twitter.com/eps696'>vadim epstein</a> \
+                        <li><a href='https://www.google.com/search?q=norman+stansfield+cornish&client=firefox-b-d&sxsrf=AJOqlzWIgeYTMY2keXJSGlpgFNwvLkdUDg:1679677352933&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjHxdXfhfX9AhVHh1wKHZZkB-UQ_AUoAXoECAEQAw&biw=1857&bih=967&dpr=1'>Norman Stansfield Cornish</a> \
+                        <li><a href='https://www.instagram.com/liskaflower/'>Alisa Lariushkina</a> \
+                        <li><a href='https://twitter.com/revrart'>revrart</a> \
+                        </ul> \
+                </div> \
+            ",
+            tags: ["Notes"]
+        },
+        {
             id: "12",
             date: "07 Oct 2020",
             title: "Are non-carbon based lifeforms possible?",
@@ -1026,9 +1191,27 @@ const initState = {
 
     albumReviews: [
         {
-            rymLink: "https://rateyourmusic.com/release/album/yes/fly-from-here/",
+            linkRym: "https://rateyourmusic.com/release/album/yes/fly-from-here/",
+            linkYoutube: "https://www.youtube.com/watch?v=SEs-i9kMpKg",
+            artistName: "Yes",
+            albumName: "Fly From Here",
+            yearOfRelease: "2011",
+            genres: ["Progressive rock", "Symphonic prog", "Progressive pop"],
             myRating: "10",
-            myReview: "",
+            myReview: "Truly one of the most grandiose, epic, uplifting musical experiences of my life. Every song absolutley bangs, flows perfectly, and as an album experience it just all feels so cohesive and well-crafted. Solitaire is undoubetley one of the weaker tracks on the album, but its not bad, better than most acoustic tracks you find on any other prog album in my opinion. It will forever baffle me why more people don't adore this album as much as I do. Shame they had to go and ruin that with the shitty 'remaster' in 2018 with no Benoît David.",
+            myTrackRatings: {
+                "Fly From Here: Overture": "9.0",
+                "Fly From Here, Part I: We Can Fly": "10.0",
+                "Fly From Here, Part II: Sad Night At The Air Field": "10.0",
+                "Fly From Here, Part III: Madman At The Scenes": "10.0",
+                "Fly From Here, Part IV: Bumpy Ride": "10.0",
+                "Fly From Here, Part V: We Can Fly (Reprise)": "10.0",
+                "The Man You Always Wanted To Be": "10.0",
+                "Life On A Film Set": "10.0",
+                "Hour Of Need": "10.0",
+                "Solitaire": "8.0",
+                "Into The Storm": "9.5"
+            }
         }
     ],
 
