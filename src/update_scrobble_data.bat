e84e72a7bd67668b9d7c42560435a45182ac561f
Nathan Steele <nbofnimachine@gmail.com>
2025-02-04 18:20:27 +0530

    [...Updating scrobble data...]

--- src/update_scrobble_data.bat
+++ src/update_scrobble_data.bat
@@ -2,6 +2,8 @@ REM For context, this script is set to run whenever I login to this laptop
 REM See the 'Task Scheduler' on my laptop for my info
 REM The task is called "Get new scrobble data" which automates the running of the python script to get my newest scrobble data
 
+set ./components/music/python_stuff/regenerate_lists_from_lastfm_userdata.py
+
 call git add .
 call git commit -m "[...Updating scrobble data...]"
 call git push origin master
