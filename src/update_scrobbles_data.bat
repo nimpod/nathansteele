af9ac2dd17315824c8d7595f0f17a387ffd49c3a
Nathan Steele <nbofnimachine@gmail.com>
2025-02-04 20:49:24 +0530

    rtoejtb

--- src/update_scrobbles_data.bat
+++ src/update_scrobbles_data.bat
@@ -2,9 +2,11 @@ REM For context, this script is set to run whenever I login to this laptop
 REM See the 'Task Scheduler' on my laptop for my info
 REM The task is called "Get new scrobble data" which automates the running of the python script to get my newest scrobble data
 
+cd C:\dev\nathansteele\src\components\music\python_stuff\
+
 python C:\dev\nathansteele\src\components\music\python_stuff\regenerate_scrobbles_data.py
 
-call git add .
-call git commit -m "[...Updating scrobbles data...]"
-call git push origin master
-call npm run deploy
+REM call git add .
+REM call git commit -m "[...Updating scrobbles data...]"
+REM call git push origin master
+REM call npm run deploy
