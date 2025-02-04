f5e4f493c9b8ee94e070433f21925072c433fd06
Nathan Steele <nbofnimachine@gmail.com>
2025-02-04 18:28:29 +0530

    [...Updating scrobble data...]

--- src/update_scrobble_data.bat
+++ src/update_scrobble_data.bat
@@ -3,7 +3,6 @@ REM See the 'Task Scheduler' on my laptop for my info
 REM The task is called "Get new scrobble data" which automates the running of the python script to get my newest scrobble data
 
 python C:\dev\nathansteele\src\components\music\python_stuff\regenerate_top_albums_data.py
-pause
 
 call git add .
 call git commit -m "[...Updating scrobble data...]"
