8d349d98f90a1e64879dcaf2c695ffe840f47640
Nathan Steele <nbofnimachine@gmail.com>
2025-03-20 21:21:55 +0530

    stuff

--- src/reducers/localData/albums/content/andreas_vollenweider/down_to_the_moon/review.jsx
+++ src/reducers/localData/albums/content/andreas_vollenweider/down_to_the_moon/review.jsx
@@ -45,21 +45,21 @@ export const review = () => (
                 Andreas Vollenweider is a harpist from Switzerland. <strong>Down To The Moon</strong> was his 5th album, released in 1986. He has a vast catalogue, all centering around the 'New Age' genre, with the harp being his primary instrument. I discovered his music just browsing the New Age genre on RYM.
             </p>
             <p>
-                The sound design on this album is immaculate. There are so many layers to each song, the harp, the flute, the drums, and many other instruments you can hear throughout the album.
+                The sound design on this album is immaculate. There are so many layers to each song, the harp, the flute, the drums, and so many other instruments you can hear throughout the album.
             </p>
             <p>
-                His music is relatively well-known in the New Age genre, but this album crosses into many other genres (jazz; prog pop; ambient). For fans of ambient music, Vollenweider's music must be experienced. This album, and Vollenweider's music in general, deserve so much more attention. He is still releasing new music to this day. In my opinion, this album is his greatest creation.
+                His music is relatively well-known in the New Age genre, but this album crosses into many other genres (jazz; prog pop; ambient). For fans of ambient music, Vollenweider's music must be experienced. This album, and Vollenweider's music in general, deserves so much more attention. He is still releasing new music to this day. In my opinion, this album is his greatest creation.
             </p>
             <p>
-                <strong>Moon Dance</strong> is an absolute banger, an amazing intro to the album that keeps new listeners captivated. <strong>Steam Forest</strong>, and <strong>Quiet Observer</strong>, are beautiful more ambient-focused. It's hard to pick a favourite song from the album, but <strong>Night Fire Dance</strong> is an absolute tune. Makes me feel like I am exploring a 13th-century mystical alchemy lab trying to find ingredients for a potion. It has a mysterious, energetic, unique vibe. <strong>Silver Wheel</strong> feels like an undiscovered song from the Donkey Kong soundtrack.
+                <strong>Moon Dance</strong> is an absolute banger, an amazing intro to the album that keeps new listeners captivated. <strong>Steam Forest</strong>, and <strong>Quiet Observer</strong>, are beautifully more ambient-focused. It's hard to pick a favorite song from the album, but <strong>Night Fire Dance</strong> is an absolute tune. It makes me feel like I am exploring a 13th-century mystical alchemy lab trying to find ingredients for a potion. It has a mysterious, energetic, unique vibe. <strong>Silver Wheel</strong> feels like an undiscovered song from the Donkey Kong soundtrack.
             </p>
             <p>
-                The first 7 songs on this album flow into each other so perfectly. I feel like the latter half of this album is slightly weaker (track 8 onwards), it feels a bit less layered to my ears. Albeit it is just 4 weaker tracks (in my opinion), which does unfortunately stop this from being a 10/10 album (in my opinion). But the overall vibe of this album is wonderful. So many majestic moments of pure joy.
+                The first 7 songs on this album flow into each other so perfectly. I feel like the latter half of this album is slightly weaker (track 8 onwards); it feels a bit less layered to my ears. Albeit it is just 4 weaker tracks (in my opinion), which does unfortunately stop this from being a 10/10 album (in my opinion). But the overall vibe of this album is wonderful. So many majestic moments of pure joy.
             </p>
             <p>
-                Here is a wonderful live performance of <strong>Night Fire Dance</strong>:
+                Here is a wonderful live performance of <strong>Night Fire Dance</strong>: 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/pqWfIHmJIb0?si=QYHOJwnt0QIk6flL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/pqWfIHmJIb0?si=QYHOJwnt0QIk6flL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
 
         {/* My ratings of each track */}
